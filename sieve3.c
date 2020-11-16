f8f60fbac9349964af7aa346d2617b605dc5f901
Chi Chiu <ctsan005@ucr.edu>
2020-11-15 20:21:34 -0800

    first

--- sieve3.c
+++ sieve3.c
@@ -153,7 +153,7 @@ int main (int argc, char *argv[])
    while(i <= high_value){
       for(j = 0; j < num_prime - 2; j++){
          while(first_list[j] < i){
-            if(first_list[j] < high_value){
+            if(first_list[j] < size){
                marked[first_list[j]] = 1;
                first_list[j] += prime_list[j];
             }
