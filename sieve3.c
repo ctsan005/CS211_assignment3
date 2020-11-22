676c89d9984ec283761e14ff4d1991cf6171a851
Chi Chiu <ctsan005@ucr.edu>
2020-11-22 15:05:53 -0800

    sieve3 test version

--- sieve3.c
+++ sieve3.c
@@ -131,7 +131,7 @@ int main (int argc, char *argv[])
    f = (unsigned long int *) malloc(8*prime_size);
 
    int block_size;
-   block_size = 3000;
+   block_size = 4500;
 
    // register unsigned long int a0;
    // register unsigned long int a1;
