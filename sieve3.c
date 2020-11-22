16d26b90169f5bf3be8f043b7b5c0e0a882a938a
Chi Chiu <ctsan005@ucr.edu>
2020-11-22 02:13:55 -0800

    sieve3 test version

--- sieve3.c
+++ sieve3.c
@@ -179,7 +179,7 @@ int main (int argc, char *argv[])
       //    //if the list size reached 100, start to do the marked
          if(list_size%2 == 0){
 
-            i = low_value + 8;
+            i = low_value + 500;
             while(i <= high_value){
                
                while(f0 < i && f0 < size){
@@ -196,7 +196,7 @@ int main (int argc, char *argv[])
                   i++;
                }
                else{
-                  i = ((i + 8) > high_value ) ? high_value : i + 8;
+                  i = ((i + 500) > high_value ) ? high_value : i + 500;
                }
             }
 
